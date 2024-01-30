2b08fdf059d3690ea59a3aa5748ce7608575b029
novgov <158186617+novgov@users.noreply.github.com>
2024-01-30 19:08:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -182,4 +182,4 @@ def project():
 #     return render_template("bisnes.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4567, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
